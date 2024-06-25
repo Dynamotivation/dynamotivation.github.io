eac2e3df437d02bfbe1d7ce15b30f51ad6a66d36
Dynamotivation <github@dynamotivation.com>
2024-06-25 23:10:06 +0200

    Scroll Indicator Prototype

--- static/js/blogPage.js
+++ static/js/blogPage.js
@@ -1,28 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var pageTitle = document.querySelectorAll('#pageTitle');
+    // Select elements
+    let pageTitle = document.getElementById('pageTitle');
+    let jsOnlyElements = document.querySelectorAll('.jsOnly');
+    let progressHr = document.getElementById('progressHr');
+    let stickyContainer = document.getElementById('stickyContainer');
 
-    var jsOnlyElements = document.querySelectorAll('.jsOnly');
-
-    pageTitle[0].style.marginBottom = "1.9rem"; //Subtract height of hr + top margin
+    // Unhide elements without layout shifting
+    pageTitle.style.marginBottom = "0";
     jsOnlyElements.forEach(function (element) {
         element.style.display = "block";
     });
 
-    let title = document.getElementById('pageTitle');
-    let progressHr = document.getElementById('progressHr');
-
+    // Resize separator to initially be slightly wider than the title
     function setHRWidth() {
-        const titleWidth = title.offsetWidth;
-        progressHr.style.width = `${titleWidth + convertRemToPixels(3.2)}px`;
+        if (!stickyContainer.classList.contains('isSticky')) {
+            const titleWidth = pageTitle.offsetWidth;
+            progressHr.style.width = `${titleWidth + convertRemToPixels(3.2)}px`;
+        }
     }
 
-    // Set the width initially
     setHRWidth();
-
-    // Update the width on window resize
     window.addEventListener('resize', setHRWidth);
+
+    // Transition separator to progress bar
+    const observer = new IntersectionObserver(
+        ([e]) => {
+            e.target.classList.toggle('isSticky', e.intersectionRatio < 1)
+            
+            if (e.intersectionRatio < 1) {
+                progressHr.style.width = `${stickyContainer.offsetWidth + convertRemToPixels(0.4)}px`
+            }
+            else {
+                setHRWidth();
+            }
+        },
+        { threshold: [1] }
+    );
+
+    observer.observe(stickyContainer)
 });
 
-function convertRemToPixels(rem) {    
+function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
